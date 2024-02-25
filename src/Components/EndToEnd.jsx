c289e79fe05b5f71ac8248e6558300a18cd71957
Abdulmalik <ayodejiamzat@gmail.com>
2024-02-25 17:55:01 +0100

    first

--- src/Components/EndToEnd.jsx
+++ src/Components/EndToEnd.jsx
@@ -13,12 +13,12 @@ const EndToEnd = () => {
                 <div className='col-md-4 pt-4 text-center'>
                     <h2 className='data mt-5'>Data</h2>
                     <img src={chart} className='myimagee2e mt-3'/>
-                    <p className='datais mt-5'>Data is the new currency. Future success is defined by how well organisations can harness their data, learn from it and use it to inform their future. Our data people will work with you to define your data strategy and implement the best data solutions, from AI and robotics, to Master Data Managment to data platfoms.</p>
+                    <p className='datais mt-5'>Data is the new currency. Future success is defined by how well organizations can harness their data, learn from it and use it to inform their future. Our data people will work with you to define your data strategy and implement the best data solutions, from AI to Master Data Management to data platforms.</p>
                 </div>
                 <div className='col-md-4 pt-4 text-center'>
                     <h2 className='data mt-5'>Technology</h2>
                     <img src={tech} className='myimagee2e mt-3'/>
-                    <p className='datais mt-5'>echnology has reshaped the business world and continues to be the biggest driver of change. Our technologists are cutting edge, with in-depth knowledge of the latest developer-led, low-code or no-code solutions and applications.</p>
+                    <p className='datais mt-5'>Technology has reshaped the business world and continues to be the biggest driver of change. Our technologists are cutting edge, with in-depth knowledge of the latest developer-led, low-code or no-code solutions and applications.</p>
                 </div>
                 <div className='col-md-4 pt-4 text-center'>
                     <h2 className='data mt-5'>Consulting</h2>
